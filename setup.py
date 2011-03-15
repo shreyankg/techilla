5140f59c2b31796ffc7f13db63beb2eafa3379b5
Shreyank Gupta <sgupta@redhat.com>
2011-03-15 15:54:27 +0530

    Removed tests.py. Added setup.py.

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#
+# Copyright 2011 Red Hat Inc.
+# Author: Shreyank Gupta <sgupta@redhat.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.  See
+# http://www.gnu.org/copyleft/gpl.html for the full text of the
+# license.
+#
+"Tequilla Bugzilla XMLRPC client"
+
+
+from distutils.core import setup
+from distutils.sysconfig import get_python_lib
+
+doclines = __doc__.split("\n")
+
+setup(
+    name='tequilla',
+    version='0.1',
+    description=doclines[0],
+    long_description="\n".join(doclines[:]),
+    platforms=["Linux"],
+    author='Shreyank Gupta',
+    author_email='sgupta@redhat.com',
+    url='',
+    license='http://www.gnu.org/licenses/old-licenses/gpl-2.0.html',
+    packages=['bz_xmlrpc'],
+)
